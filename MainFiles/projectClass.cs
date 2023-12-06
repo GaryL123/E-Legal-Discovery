71fa3887075170491a0dbd7aced0a2f305493bb4
GaryL123 <126824940+GaryL123@users.noreply.github.com>
2023-12-06 17:56:25 -0500

    Add files via upload

---
+++ MainFiles/projectClass.cs
@@ -0,0 +1,84 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+
+public class Project
+{
+    // Public properties
+    public int ProjectId { get; set; }
+    public string ProjectName { get; set; }
+    public string Description { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int OwnerId { get; set; }
+
+    // Constructor
+    public Project()
+    {
+        // Initialize default values
+        ProjectName = string.Empty;
+        Description = string.Empty;
+        StartDate = DateOnly.MinValue;
+        EndDate = DateOnly.MinValue;
+        CreatedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+        OwnerId = 0;
+    }
+
+    // Method to save the project to the database
+    public void SaveToDatabase(string connectionString)
+    {
+        using (var conn = new NpgsqlConnection(connectionString))
+        {
+            conn.Open();
+
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO projects (project_name, description, start_date, end_date, created_at, updated_at, owner_id) " +
+                                  "VALUES (@project_name, @description, @start_date, @end_date, @created_at, @updated_at, @owner_id) RETURNING project_id";
+
+                cmd.Parameters.AddWithValue("project_name", ProjectName);
+                cmd.Parameters.AddWithValue("description", Description);
+                cmd.Parameters.AddWithValue("start_date", StartDate);
+                cmd.Parameters.AddWithValue("end_date", EndDate);
+                cmd.Parameters.AddWithValue("created_at", CreatedAt);
+                cmd.Parameters.AddWithValue("updated_at", UpdatedAt);
+                cmd.Parameters.AddWithValue("owner_id", OwnerId);
+
+                // Retrieve the generated project_id after insertion
+                ProjectId = (int)cmd.ExecuteScalar();
+            }
+
+            conn.Close();
+        }
+    }
+
+    // Static method to get project_id by project name
+    public static int GetProjectIdByName(string projectName, string connectionString)
+    {
+        using (var conn = new NpgsqlConnection(connectionString))
+        {
+            conn.Open();
+
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT project_id FROM projects WHERE project_name = @project_name";
+                cmd.Parameters.AddWithValue("project_name", projectName);
+
+                var result = cmd.ExecuteScalar();
+
+                if (result != null && result != DBNull.Value)
+                {
+                    return (int)result;
+                }
+            }
+
+            conn.Close();
+        }
+        return -1;
+    }
+}
---
+++ MainFiles/startup.cs
@@ -0,0 +1,50 @@
+﻿class Startup
+{
+    static void Main()
+    {
+        string connectionString = "Host=localhost;Username=postgres;Password=Toor11!!;Database=postgres";
+        User currentUser = null; // Initialize currentUser
+
+        // Keep the application running until the user decides to log out or close the program
+        while (true)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Log In / Log Out");
+            Console.WriteLine("2. Sign Up");
+            Console.WriteLine("3. Create Project");
+            Console.WriteLine("4. Exit");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        User.LogInOut(connectionString, ref currentUser);
+                        break;
+                    case 2:
+                        User.SignUp(connectionString, currentUser);
+                        break;
+                    case 3:
+                        if (currentUser != null)
+                            currentUser.CreateProject(connectionString);
+                        
+                        else
+                            Console.WriteLine("You need to log in first.");
+                        
+                        break;
+                    case 4:
+                        Console.WriteLine("Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+}
